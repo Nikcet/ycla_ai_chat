cd3ee48b4f30d1661898f5be4f0d7a8c6d2b3a64
Nikcet <mstislav.kr@yandex.ru>
2025-05-22 14:53:37 +0300

    new settings for logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 from loguru import logger
+import sys
+
+logger.remove()
+
+logger.add(
+    sink=sys.stdout,
+    level="INFO",
+    colorize=True,
+    format="{time:DD.MM.YY — HH:mm:ss} | <level>{level}</level> | <yellow>{file}</yellow> : <cyan>{line}</cyan> | {message}",
+)
 
 logger.add(
     "logs.log",
     rotation="10 MB",
-    retention="15 days",
+    retention="2 days",
     compression="zip",
-    level="INFO"
+    level="INFO",
+    format="{time:DD.MM.YY — HH:mm:ss} | {level} | {file}:{line} | {message}",
 )
 
 __all__ = ["logger"]
