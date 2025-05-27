1ab65602e54686ac731edbc7bda9e9e1ebdb6cfb
Nikcet <mstislav.kr@yandex.ru>
2025-05-27 22:46:34 +0300

    refactor

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,5 @@
-from pydantic import BaseModel, Field
-from typing import Optional, List, Dict
-# from datetime import datetime
-# from sqlmodel import SQLModel, Field, create_engine, Session, select
+from pydantic import BaseModel
+from typing import List
 
 
 class RegisterRequest(BaseModel):
@@ -22,62 +20,3 @@ class ChatRequest(BaseModel):
 
 class ChatResponse(BaseModel):
     answer: str
-
-
-# class Company(SQLModel, table=True):
-#     id: Optional[int] = Field(default=None, primary_key=True)
-#     name: str = Field(index=True)
-#     api_key: str = Field(index=True, unique=True)
-
-# # Для Company
-# class CompanyCreate(BaseModel):
-#     name: str = Field(..., max_length=100)
-
-
-# class CompanyResponse(CompanyCreate):
-#     id: str
-#     name: str = Field(..., max_length=100)
-#     created_at: datetime
-
-#     class Config:
-#         orm_mode = True
-
-
-# # Для Admin
-# class AdminCreate(BaseModel):
-#     username: str = Field(..., min_length=3, max_length=50)
-#     company_id: str
-
-
-# class AdminResponse(AdminCreate):
-#     id: str
-#     created_at: datetime
-
-#     class Config:
-#         orm_mode = True
-
-
-# # Для Chat
-# class Message(BaseModel):
-#     content: str
-#     role: str
-
-
-# class ChatCreate(BaseModel):
-#     company_id: str
-#     initial_message: Optional[str] = None  # если нужно сразу создать первое сообщение
-
-
-# class ChatResponse(BaseModel):
-#     id: str
-#     company_id: str
-#     messages: Dict[Message]  # или Dict если предпочитаете JSON-объект
-#     created_at: datetime
-#     updated_at: datetime
-
-#     class Config:
-#         orm_mode = True
-
-
-# class ApiKey(CompanyResponse):
-#     api_key: str
