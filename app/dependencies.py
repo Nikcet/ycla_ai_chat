1182d70f02ede2e94bc6c8d326459fb5202dae3e
Nikcet <mstislav.kr@yandex.ru>
2025-05-27 22:46:43 +0300

    refactor

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,13 +1,12 @@
 from fastapi import Header, HTTPException, Depends
-from uuid import uuid4
-from sqlmodel import Session, select
-from typing import List
 
+from sqlmodel import Session, select
+
+from app import logger
 from app.models import Company
 from app.config import get_app_settings
 from app.database import engine
-from app.utils import get_embedding, chunk_text, extract_text
-from app.schemas import UploadRequest
+
 
 app_settings = get_app_settings()
 
@@ -24,27 +23,3 @@ def get_current_company(
     if not company:
         raise HTTPException(status_code=401, detail="Invalid API Key")
     return company
-
-# TODO: засунуть в задачу
-async def create_batch(req: UploadRequest, company: Company, file_path: str) -> List[dict]:
-    batch = []
-    try:
-        text = extract_text(file_path)
-    except Exception as e:
-        raise HTTPException(
-            status_code=400, detail=f"Ошибка чтения файла '{file_path}': {e}"
-        )
-
-    for chunk in chunk_text(text):
-        emb = await get_embedding(chunk)
-        batch.append(
-            {
-                "id": f"{company.id}-{uuid4()}",
-                "company_id": int(company.id),
-                "content": str(chunk),
-                "embedding": [
-                    float(x) for x in emb
-                ],  # TODO: пока работает так, надо отрефакторить
-            }
-        )
-    return batch
