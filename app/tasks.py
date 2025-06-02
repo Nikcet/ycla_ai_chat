2d7dc6020107f63d97c04d9fd65031940bf3b37f
Nikcet <mstislav.kr@yandex.ru>
2025-06-02 03:30:57 +0300

    add logs

--- app/tasks.py
+++ app/tasks.py
@@ -1,16 +1,14 @@
-from sqlmodel import Session
-# from celery import shared_task
-# from app.utils import create_batch
-# from app.database import search_client
 from app.dependencies import upload_documents, delete_documents
 from app.celery_worker import celery_tasks
-# from app import logger
+from app import logger
 
 
 @celery_tasks.task
 def upload_documents_task(documents: list[str], company_id: int) -> dict[str, bool]:
+    logger.info(f"Uploading documents to company_id: {company_id}")
     return upload_documents(documents, company_id)
 
 @celery_tasks.task
 def delete_documents_task(company_id: int) -> dict[str, bool]:
+    logger.info(f"Deleting documents to company_id: {company_id}")
     return delete_documents(company_id)
