3f6f56ba7d16498d9f30a439084bc367fd6f7315
Nikcet <mstislav.kr@yandex.ru>
2025-06-08 01:35:19 +0300

    refactor and make creating all clients globally

--- app/clients.py
+++ app/clients.py
@@ -1,34 +1,48 @@
 from openai import AsyncOpenAI
 from openai.lib.azure import AsyncAzureOpenAI
-from app import settings, logger
+from azure.core.credentials import AzureKeyCredential
 from redis import asyncio as aioredis
+from azure.search.documents import SearchClient
+from sqlmodel import create_engine
 
-azure_client = AsyncAzureOpenAI(
-    api_key=settings.api_key,
-    api_version=settings.api_version,
-    azure_endpoint=settings.endpoint,
-)
-if azure_client:
-    logger.info("Azure OpenAI client initialized successfully")
-else:
-    logger.error("Azure OpenAI client initialization failed")
+from app import settings, logger
 
-deepseek_client = AsyncOpenAI(
-    api_key=settings.deepseek_api_key,
-    base_url=settings.deepseek_url,
-)
+try:
+    azure_client = AsyncAzureOpenAI(
+        api_key=settings.api_key,
+        api_version=settings.api_version,
+        azure_endpoint=settings.endpoint,
+    )
+except Exception as e:
+    logger.error(f"Error initializing Azure client: {e}")
 
-if deepseek_client:
-    logger.info("DeepSeek client initialized successfully")
-else:
-    logger.error("DeepSeek client initialization failed")
+try:
+    deepseek_client = AsyncOpenAI(
+        api_key=settings.deepseek_api_key,
+        base_url=settings.deepseek_url,
+    )
+except Exception as e:
+    logger.error(f"Error initializing DeepSeek client: {e}")
 
-logger.info(settings.redis_port)
-redis = aioredis.from_url(
-    url=f"redis://{settings.redis_host}:{settings.redis_port}/0", decode_responses=True
-)
+try:
+    redis = aioredis.from_url(
+        url=f"redis://{settings.redis_host}:{settings.redis_port}/0",
+        decode_responses=True,
+    )
+except Exception as e:
+    logger.error(f"Error connecting to Redis: {e}")
 
-if redis:
-    logger.info("redis client initialized successfully")
-else:
-    logger.error("redis client initialization failed")
+try:
+    search_client = SearchClient(
+        endpoint=settings.search_endpoint,
+        index_name=settings.search_index,
+        credential=AzureKeyCredential(settings.search_admin_key),
+    )
+except Exception as e:
+    logger.error(f"Error connecting to Azure Cognitive Search: {e}")
+
+
+try:
+    engine = create_engine(settings.pg_url, echo=True)
+except Exception as e:
+    logger.error(f"Error connecting to PostgreSQL: {e}")
